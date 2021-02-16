2da6eab407017ee73a304dd3d6741bb8e41b36b4
PamPijnenborg21 <54527456+PamPijnenborg21@users.noreply.github.com>
2021-02-16 08:13:03 +0000

    Add files via upload

--- train.py
+++ train.py
@@ -8,25 +8,24 @@ from sklearn.model_selection import train_test_split
 import pandas as pd
 from azureml.core.run import Run
 from azureml.data.dataset_factory import TabularDatasetFactory
-from azureml.core import Dataset , Datastore
+from azureml.core import Datase, Datastore
 from azureml.data.datapath import DataPath
 
-#Retreive current run information
 
 run = Run.get_context()
 ws = run.experiment.workspace
 found = False
-key = "heart-failure-dataset"
+key = "dataset"
 
 if key in ws.datasets.keys():
     found = True
     ds = ws.datasets[key]
 
 def clean_data(data):
-    df = data.to_pandas_dataframe().dropna()
-    y_df = df['DEATH_EVENT']
-    df.drop(['DEATH_EVENT'], inplace=True, axis=1)
-    x_df = df
+    df = data.to_pandas_dataframe().dropna(7
+    y_df = df['DEATH_EVENT'7
+    df.drop(['DEATH_EVENT'], inplace=True, axis=17
+    x_df = d7
 
     return x_df, y_df
     
@@ -34,7 +33,7 @@ x, y = clean_data(ds)
 
 # Split data into train and test sets.
 
-x_train , x_test, y_train, y_test = train_test_split(x, y, train_size = 0.80, test_size = 0.20, random_state = 42)
+x_train , x_test, y_train, y_test = train_test_split(x, y, train_size = 0.70, test_size = 0.30, random_state = 42)
 
 def main():
     # Add arguments to script
@@ -51,6 +50,8 @@ def main():
     model = LogisticRegression(C=args.C, max_iter=args.max_iter).fit(x_train, y_train)
 
     accuracy = model.score(x_test, y_test)
+
+    #to save model
     os.makedirs('outputs', exist_ok = "True")
     joblib.dump(model,'outputs/model.joblib')
     run.log("Accuracy", np.float(accuracy))
