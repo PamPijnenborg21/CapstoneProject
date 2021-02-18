4773e54908c1b40dc2ddeb809a88a0bd9cb626f5
PamPijnenborg21 <54527456+PamPijnenborg21@users.noreply.github.com>
2021-02-18 13:02:24 +0000

    Add files via upload

---
+++ train (2).py
@@ -0,0 +1,58 @@
+from sklearn.linear_model import LogisticRegression
+import argparse
+import os
+import numpy as np
+from sklearn.metrics import mean_squared_error
+import joblib
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import OneHotEncoder
+import pandas as pd
+from azureml.core.run import Run
+from azureml.data.dataset_factory import TabularDatasetFactory
+
+
+def clean_data(data):
+    df = data.to_pandas_dataframe().dropna()
+    y_df = df['DEATH_EVENT']
+    df = df.drop(['DEATH_EVENT'], axis=1)
+    x_df = df
+    return x_df, y_df
+
+from azureml.core import Workspace, Dataset
+
+from azureml.core.run import Run
+
+run = Run.get_context()
+workspace = run.experiment.workspace
+
+ds = Dataset.get_by_name(workspace, name='dataset')
+
+x, y = clean_data(ds) 
+
+x_train, x_test, y_train, y_test = train_test_split(x, y)
+
+
+
+def main():
+    # Add arguments to script
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--C', type=float, default=1.0, help="Inverse of regularization strength. Smaller values cause stronger regularization")
+    parser.add_argument('--max_iter', type=int, default=100, help="Maximum number of iterations to converge")
+
+    args = parser.parse_args()
+
+    run.log("Regularization Strength:", np.float(args.C))
+    run.log("Max iterations:", np.int(args.max_iter))
+
+    model = LogisticRegression(C=args.C, max_iter=args.max_iter).fit(x_train, y_train)
+
+    accuracy = model.score(x_test, y_test)
+    run.log("accuracy", np.float(accuracy))
+
+    #To save model
+    os.makedirs('outputs', exist_ok = True)
+    joblib.dump(model, 'outputs/model.joblib')
+
+if __name__ == '__main__':
+    main()
